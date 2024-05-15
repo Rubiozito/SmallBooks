3ecf9c19f6b99d9bf14868c73b2a936f62e72da3
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-05-15 01:13:35 -0300

    app express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Curso de express API",
-  "/livros": "Rota Livros",
-  "/autores": "Rota Autores",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Servidor online");
 });
