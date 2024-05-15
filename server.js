9bdf96e241c5ac3dac09391b2c8798a93722d711
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-05-15 00:24:23 -0300

    nodemon e rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de express API",
+  "/livros": "Rota Livros",
+  "/autores": "Rota Autores",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
